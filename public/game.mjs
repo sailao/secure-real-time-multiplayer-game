158e13baa6b7571c7d9237bafdd6e046b09480aa
sailao <sailaohsang@gmail.com>
2021-07-09 20:58:17 +0630

    unfinish

--- public/game.mjs
+++ public/game.mjs
@@ -1,6 +1,14 @@
-import Player from './Player.mjs';
-import Collectible from './Collectible.mjs';
+// import Player from './Player.mjs';
+// import Collectible from './Collectible.mjs';
 
-const socket = io();
+// const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+var mainImg = new Image()
+mainImg.src = '../public/image/main-player.png'
+mainImg.onload = () => context.drawImage(mainImg, 100, 100, 30, 30)
+
+var otherImg = new Image()
+otherImg.src = '../public/image/other-player.png'
+otherImg.onload = () => context.drawImage(otherImg, 200, 100, 30, 30)
--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expect = require('chai');
 const socket = require('socket.io');
+const helmet = require("helmet");
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
 
 const app = express();
+
+app.use(helmet.hidePoweredBy({"setTo": "PHP 7.4.3"}))
+app.use(helmet.xssFilter())
+app.use(helmet.noCache())
+app.use(helmet.noSniff())
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
